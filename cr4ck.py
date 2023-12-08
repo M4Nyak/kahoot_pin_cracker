8c44be21477915cc3e2dfde35b5e06ccf0e80abc
M4Nyak <119705619+M4Nyak@users.noreply.github.com>
2023-12-08 20:08:10 +0300

    Create cr4ck.py

---
+++ cr4ck.py
@@ -0,0 +1,21 @@
+import requests
+import random
+import string
+sayi_kumesi= string.digits
+while True:
+ pin = str(random.choice(sayi_kumesi)) + str(random.choice(sayi_kumesi)) + str(random.choice(sayi_kumesi)) + str(random.choice(sayi_kumesi)) + str(random.choice(sayi_kumesi)) + str(random.choice(sayi_kumesi)) + str(random.choice(sayi_kumesi))
+ rs= requests.session()
+ rs.headers = {
+'user-agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 7_1_1; en-US) AppleWebKit/602.31 (KHTML, like Gecko) Chrome/52.0.1856.275 Safari/537'
+
+
+ }
+ url= f"https://kahoot.it/reserve/session/{pin}/?1702051885232"
+ a= rs.get(url).text
+ rs.close()
+ if 'Not found' == a:
+    print("ders bulunamadi: ",pin)
+ else:
+    print(url)
+    print("ders bulundu: ",pin)
+    open("bulunandersler.txt","w").write(f"{pin} \n")
